6b1d738d5d0ded14d0805207d2cd7d15345bae70
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-01 09:25:49 -0700

    First real test

--- test/r00lz_test.rb
+++ test/r00lz_test.rb
@@ -5,7 +5,8 @@ class R00lzTest < Minitest::Test
     refute_nil ::R00lz::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_app_returns_success
+    env = { "PATH_INFO" => "/", "QUERY_STRING" => "" }
+    assert_equal 200, ::R00lz::App.new.call(env)[0]
   end
 end
