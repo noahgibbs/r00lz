ab1e99f1ec0406c62b104f2455068551ddbb888d
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-01 14:36:38 -0700

    Add new-style controllers

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -2,9 +2,26 @@ require "r00lz/version"
 module R00lz
   class App
     def call(env) # Like proc#call
+      kl, act = cont_and_act(env)
+      text = kl.new(env).send(act)
       [200,
        {'Content-Type' => 'text/html'},
-       ["Hello from R00lz!"]]
+       [text]]
+    end
+
+    def cont_and_act(env)
+      _, con, act, after =
+        env["PATH_INFO"].split('/', 4)
+      con = con.capitalize +
+        "Controller"
+      [Object.const_get(con), act]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+    def initialize(env)
+      @env = env
     end
   end
 end
