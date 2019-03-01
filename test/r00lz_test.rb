627604354dc42c65b93d8a96670db5070d4f3158
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-01 14:47:28 -0700

    Alter test to use new-style controller

--- test/r00lz_test.rb
+++ test/r00lz_test.rb
@@ -1,12 +1,18 @@
 require "test_helper"
 
+class PostsController < R00lz::Controller
+  def speculate
+    "Whoah, man..."
+  end
+end
+
 class R00lzTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::R00lz::VERSION
   end
 
-  def test_app_returns_success
-    env = { "PATH_INFO" => "/", "QUERY_STRING" => "" }
+  def test_new_controller_action
+    env = { "PATH_INFO" => "/posts/speculate", "QUERY_STRING" => "" }
     assert_equal 200, ::R00lz::App.new.call(env)[0]
   end
 end
