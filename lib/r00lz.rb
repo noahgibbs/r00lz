cd9a7e056ae038725844a467915acb67621d5b2d
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-20 14:46:41 -1000

    Add 'request' and 'params' methods for controllers

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -30,6 +30,14 @@ module R00lz
       e = ERB.new(File.read template)
       e.result(b)
     end
+
+    def request
+      @request ||= Rack::Request.new @env
+    end
+
+    def params
+      request.params
+    end
   end
 
   def self.to_underscore(s)
